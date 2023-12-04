0a68063742c65051c66c43066e32667bc91b6b74
wzwtt <wzwttol@outlook.com>
2023-12-04 10:49:59 +0800

    commit

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -29,10 +29,7 @@ export function Menu() {
         <div className="background"></div>
         <div className="menu-description">
           <div className="description">
-            <p>免责声明：</p>
-            <p>本网站是一个纯粹的技术示例，旨在展示和分享我们的技术能力。网站的设计和内容受到《原神》的启发，并尽可能地复制了《原神》的登录界面。我们对此表示敬意，并强调这个项目不是官方的《原神》产品，也没有与《原神》或其母公司miHoYo有任何关联。</p>
-            <p>我们没有，也无意从这个项目中获得任何经济利益。这个网站的所有内容仅供学习和研究目的，以便让更多的人了解和熟悉webgl开发技术。</p>
-            <p>如果miHoYo或任何有关方面认为这个项目侵犯了他们的权益，请联系我们，我们会立即采取行动。</p>
+
           </div>
         </div>
         <button className="ClickMe" onClick={login ? () => {
@@ -40,12 +37,7 @@ export function Menu() {
           gameManager.emit("start")
           setLogin(false)
         } : () => { }}></button>
-        <button className="ClickMe" style={{
-          bottom: "16%",
-          backgroundImage: `url("/Genshin/jump.png")`
-        }} onClick={login ? () => {
-          window.open('https://www.bilibili.com/video/BV1E8411v7xy');
-        } : () => { }}></button>
+      
       </div>
       {doorCreate && (
         <div className="menu-doorCreate-content" style={{ opacity: openDoor ? "0" : "1" }}>
